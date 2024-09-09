187fa2c10d9695654e178be94126f929dba619aa
MEW <contactmeganwalsh@gmail.com>
2024-09-09 16:11:13 -0400

    added navigation codestitch, deleted css and js

--- app.js
+++ app.js
@@ -1,45 +1,35 @@
-/****************************************************************************/
-const navbar = document.querySelector('#navbar');
-const navbarMenu = navbar.querySelector('#navbar .menu');
-const navbarItems = navbar.querySelectorAll('#navbar .menu .item');
-const navbarToggle = navbar.querySelector('#navbar .toggle');
-const body = document.querySelector('body');
-// Open
-function openNavbar() {
-	navbarMenu.classList.add('active');
-	navbar.classList.add('open');
-	navbarToggle.setAttribute('aria-label', 'Close navigation menu.');
-}
-function closeNavbar() {
-	navbarMenu.classList.remove('active');
-	navbar.classList.remove('open');
-	navbarToggle.setAttribute('aria-label', 'Open navigation menu.');
-}
-// Toggle drawer menu
-navbarToggle.addEventListener('click', () => {
-	if (navbar.classList.contains('open')) {
-		closeNavbar();
-	} else {
-		openNavbar();
-	}
-});
-// Close nav drawer upon user selection
-navbarItems.forEach(item =>
-	item.addEventListener('click', () => {
-		if (navbar.classList.contains('open')) closeNavbar();
-	})
-);
+// add classes for mobile navigation toggling
+var CSbody = document.querySelector('body');
+const CSnavbarMenu = document.querySelector('#cs-navigation');
+const CShamburgerMenu = document.querySelector('#cs-navigation .cs-toggle');
 
-// Close nav drawer when screen is resized
-window.addEventListener('resize', () => {
-	closeNavbar();
+CShamburgerMenu.addEventListener('click', function () {
+	CShamburgerMenu.classList.toggle('cs-active');
+	CSnavbarMenu.classList.toggle('cs-active');
+	CSbody.classList.toggle('cs-open');
+	// run the function to check the aria-expanded value
+	ariaExpanded();
 });
-document.addEventListener('click', e => {
-	if (
-		e.target !== navbarToggle &&
-		e.target !== navbarMenu.classList.contains('active') &&
-		e.target !== e.target.closest('#navbar .menu')
-	)
-		closeNavbar();
-});
-/****************************************************************************/
+
+// checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not
+function ariaExpanded() {
+	const csUL = document.querySelector('#cs-expanded');
+	const csExpanded = csUL.getAttribute('aria-expanded');
+
+	if (csExpanded === 'false') {
+		csUL.setAttribute('aria-expanded', 'true');
+	} else {
+		csUL.setAttribute('aria-expanded', 'false');
+	}
+}
+
+// mobile nav toggle code
+const dropDowns = Array.from(
+	document.querySelectorAll('#cs-navigation .cs-dropdown')
+);
+for (const item of dropDowns) {
+	const onClick = () => {
+		item.classList.toggle('cs-active');
+	};
+	item.addEventListener('click', onClick);
+}
